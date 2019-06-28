486c9b8a88c3ed92ed1897b93bbd3f1a6e517a83
Lipatan ID <lipatanid@gmail.com>
2019-06-28 14:20:03 +0700

    a

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//* Add PayPal Email
+simpleCart({
+  checkout: {
+    type: "PayPal",
+    email: "iryanto.heru@gmail.com"
+  }
+});
+
+//* Add shopping cart dropdown in header
+jQuery(document).ready(function () {
+  $('.showCart').on('click', function () {
+    $('#cartPopover').slideToggle('fast');
+    $('.showCart span.dropdown').toggleClass('fa-chevron-circle-down fa-chevron-circle-up');
+  })
+});
+
+//* Define spreadsheet URL (make sure you add the #gid=0 for the sheet you want to use)
+var googleSheetURI = 'https://docs.google.com/spreadsheets/d/14-2KOZ0uYqdrGNz24C2ZtQ6tLM3SARLsf1lAVljJq0I/edit#gid=0';
+
+//* Compile the Handlebars template for HR leaders
+var HRTemplate = Handlebars.compile($('#hr-template').html());
+
+//* Load products from spreadsheet
+$('#products').sheetrock({
+  url: googleSheetURI,
+  query: "select A,B,C,D,E",
+  rowTemplate: HRTemplate
+});
